df07fb18eb891875a8066a5338b14415c12865e8
carlo-baron <carlobaron430@gmail.com>
2025-05-06 09:55:36 +0800

    Readme changes

--- obj/Debug/net8.0/To-Do-List (MVC).AssemblyInfo.cs
+++ obj/Debug/net8.0/To-Do-List (MVC).AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("To-Do-List (MVC)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b158a821498cdec935bf771026913740208be3f")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bba2ad877bdda3a4619ef05db857c062b57ccc19")]
->>>>>>> hotfix/UI
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8994728a293aeba6ca9f41c2edec3ff6240a7e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("To-Do-List (MVC)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("To-Do-List (MVC)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
